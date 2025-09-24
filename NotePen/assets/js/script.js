b9594c2594ac2875f6c43d83f222a2c272c98219
SereywatPro <sereywat999@gmail.com>
2025-09-25 03:00:47 +0700

    update script

--- NotePen/assets/js/script.js
+++ NotePen/assets/js/script.js
@@ -1,24 +1,27 @@
 "use strict"
 
-const NewMessage = document.createElement('div');
 const btnTakeNote = document.getElementById('takeNote');
 const addThings = document.querySelector(".addThings");
 const vInput = document.querySelector('#Idea');
 
 btnTakeNote.addEventListener('click', (event) => {
     event.preventDefault();
+    const NewMessage = document.createElement('div');
     let TxtInput = vInput.value;
 
-    NewMessage.innerHTML += `<p class="box-things">${TxtInput}<button class="btn btn-danger">It's Okay😉</button></p>`;
+    NewMessage.innerHTML = `<p class="box-things">${TxtInput}<button class="btn btn-danger">It's Okay😉</button></p>`;
     
     /* modern step to modify element in DOM html */
     addThings.append(NewMessage);
 
     /* copy element which simple mean all the child elements */
     // addThings.prepend(NewMessage.cloneNode(true));
-    const btnClose = document.querySelector(".btn-danger");
+    const btnClose = NewMessage.querySelector(".btn-danger");
 
     btnClose.addEventListener('click', () => {
         NewMessage.remove();
     });
+        
+    // Clear input  
+    vInput.value = "";
 });
