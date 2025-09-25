9f0abc47ca902e4b105552b0f21f0ee27b143718
SereywatPro <sereywat999@gmail.com>
2025-09-25 18:32:59 +0700

    update script (refactoring&addLocalStorage)

--- NotePen/assets/js/script.js
+++ NotePen/assets/js/script.js
@@ -1,27 +1,42 @@
-"use strict"
+"use strict";
 
-const btnTakeNote = document.getElementById('takeNote');
+const btnTakeNote = document.getElementById("takeNote");
 const addThings = document.querySelector(".addThings");
-const vInput = document.querySelector('#Idea');
+const vInput = document.querySelector("#Idea");
+let arr = [];
+btnTakeNote.addEventListener("click", (event) => {
+  event.preventDefault();
+  let TxtInput = vInput.value;
 
-btnTakeNote.addEventListener('click', (event) => {
-    event.preventDefault();
-    const NewMessage = document.createElement('div');
-    let TxtInput = vInput.value;
-
-    NewMessage.innerHTML = `<p class="box-things">${TxtInput}<button class="btn btn-danger">It's Okay😉</button></p>`;
-    
-    /* modern step to modify element in DOM html */
-    addThings.append(NewMessage);
-
-    /* copy element which simple mean all the child elements */
-    // addThings.prepend(NewMessage.cloneNode(true));
-    const btnClose = NewMessage.querySelector(".btn-danger");
-
-    btnClose.addEventListener('click', () => {
-        NewMessage.remove();
-    });
-        
-    // Clear input  
+  if (TxtInput) {
+    arr.push(TxtInput);
+    localStorage.setItem("your writing", JSON.stringify(arr));
+    createMessage(TxtInput);
     vInput.value = "";
+  } else {
+    alert("Please Enter Idea");
+  }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  let getTxtToSave = JSON.parse(localStorage.getItem("your writing"));
+  if (getTxtToSave) {
+    getTxtToSave.forEach((note) => createMessage(note));
+  }
+});
+
+function createMessage(txt) {
+  const NewMessage = document.createElement("p");
+  NewMessage.classList.add("box-things");
+  NewMessage.innerHTML = `${txt}<button class="btn btn-danger">It's Okay😉</button>`;
+
+  addThings.append(NewMessage);
+
+  /* copy element which simple mean all the child elements 
+     addThings.prepend(NewMessage.cloneNode(true));*/
+  const btnClose = NewMessage.querySelector(".btn-danger");
+  btnClose.addEventListener("click", () => {
+    NewMessage.remove();
+    localStorage.removeItem("your writing");
+  });
+}
